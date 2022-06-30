09ab1f3db6ef4e01a0ce91d1b7ee4944e2893d50
acogange2 <acogange2@naver.com>
2022-06-30 21:32:24 +0900

    chap 5

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Counter from './component/chap3/Counter';
 import MyComponent from './component/chap3/MyComponent';
 import Say from './component/chap3/Say';
+import ValidationSample from './component/chap3/ValidationSample';
 
 function App() {
   return (
     // string: "" integer: {} 으로 props 전달
     //<MyComponent name="변백현" favoritNum={5}>웅잉</MyComponent>
     //<Counter></Counter>
-    <Say/>
+    //<Say/>
+    <ValidationSample/>
   );
 }
 
