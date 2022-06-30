845298b2c1f844870c4e6c58eb70ca24d2edbc86
acogange2 <acogange2@naver.com>
2022-06-30 17:45:26 +0900

    chap 3, Say / useState

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Counter from './component/chap3/Counter';
 import MyComponent from './component/chap3/MyComponent';
+import Say from './component/chap3/Say';
 
 function App() {
   return (
     // string: "" integer: {} 으로 props 전달
     //<MyComponent name="변백현" favoritNum={5}>웅잉</MyComponent>
-    <Counter></Counter>
+    //<Counter></Counter>
+    <Say/>
   );
 }
 
