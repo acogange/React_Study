fcdfb75c573f82ab2fd9034106c0f769ceade90b
acogange2 <acogange2@naver.com>
2022-07-01 09:22:11 +0900

    chap 5, ref

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import Counter from './component/chap3/Counter';
 import MyComponent from './component/chap3/MyComponent';
 import Say from './component/chap3/Say';
-import ValidationSample from './component/chap3/ValidationSample';
+import ValidationSample from './component/chap5/ValidationSample';
+import ScrollBox from './component/chap5/ScrollBox';
+import { render } from '@testing-library/react';
+import { Component } from 'react';
 
-function App() {
-  return (
-    // string: "" integer: {} 으로 props 전달
-    //<MyComponent name="변백현" favoritNum={5}>웅잉</MyComponent>
-    //<Counter></Counter>
-    //<Say/>
-    <ValidationSample/>
-  );
+class App extends Component {
+  render() {
+    return (
+      // string: "" integer: {} 으로 props 전달
+      //<MyComponent name="변백현" favoritNum={5}>웅잉</MyComponent>
+      //<Counter></Counter>
+      //<Say/>
+      //<ValidationSample/>
+      <div>
+      <ScrollBox ref={(ref)=>this.ScrollBox=ref}></ScrollBox>
+      <button onClick={()=>this.ScrollBox.scrollToBottom()}>go to bottom</button>
+      </div>
+    );
+  }
 }
 
 export default App;
