911fe88ff51ddc22ca56e4a52edad8054c95ef26
acogange2 <acogange2@naver.com>
2022-07-01 15:50:58 +0900

    chap 8, useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import Counter from './component/chap3/Counter';
+//import Counter from './component/chap3/Counter';
+import Counter from './component/chap8/Counter';
 import MyComponent from './component/chap3/MyComponent';
 import Say from './component/chap3/Say';
 import ValidationSample from './component/chap5/ValidationSample';
@@ -9,21 +10,15 @@ import { render } from '@testing-library/react';
 import { Component } from 'react';
 import IterationSample from './component/chap6/IterationSample';
 import LifeCycleSample from './component/chap7/LifeCycleSample';
+import Info from './component/chap8/Info';
 
 class App extends Component {
   render() {
     return (
       // string: "" integer: {} 으로 props 전달
       //<MyComponent name="변백현" favoritNum={5}>웅잉</MyComponent>
-      //<Counter></Counter>
-      //<Say/>
-      //<ValidationSample/>
-      // <div>
-      // <ScrollBox ref={(ref)=>this.ScrollBox=ref}></ScrollBox>
-      // <button onClick={()=>this.ScrollBox.scrollToBottom()}>go to bottom</button>
-      // </div>
-      //<IterationSample></IterationSample>
-      <LifeCycleSample/>
+      //<Counter/>
+      <Info/>
     );
   }
 }
